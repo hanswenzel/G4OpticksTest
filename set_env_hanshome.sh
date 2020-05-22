8f9eaabce32e2c55867fdee4e823581e43efd942
Hans Wenzel <wenzel@fnal.gov>
2020-05-22 14:07:09 -0500

    change to cuda 10.2

--- set_env_hanshome.sh
+++ set_env_hanshome.sh
@@ -3,6 +3,6 @@
 export WORK_DIR=/home/wenzel/gpu
 export OptiX_INSTALL_DIR=/home/wenzel/NVIDIA-OptiX-SDK-6.5.0-linux64
 export OPTICKS_COMPUTE_CAPABILITY=75
-export CUDA_INSTALL_DIR=/usr/local/cuda-10.1
-export CUDA_SAMPLES=/home/wenzel/NVIDIA_CUDA-10.1_Samples
+export CUDA_INSTALL_DIR=/usr/local/cuda-10.2
+export CUDA_SAMPLES=/usr/local/cuda-10.2/samples
 #------------------------------------------------------------------------------------

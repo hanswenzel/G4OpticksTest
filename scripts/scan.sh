fcd642ab70c00863b35326d1349d9f1055ad63d8
wenzel <wenzel@fnal.gov>
2020-11-21 10:20:54 -0600

    allow to switch off print outs

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -5,10 +5,27 @@
 #
 # Created on Nov 20, 2020, 10:12:41 PM
 #
-time ./G4OpticksTest G4Opticks_50.gdml muon.mac >& 50.log
-time ./G4OpticksTest G4Opticks_100.gdml muon.mac >& 100.log
-time ./G4OpticksTest G4Opticks_5000.gdml muon.mac >& 5000.log
-#
-time ./G4OpticksTest G4Opticks_50.gdml muon_noIO.mac >& 50_noIO.log
-time ./G4OpticksTest G4Opticks_100.gdml muon_noIO.mac >& 100_noIO.log
-time ./G4OpticksTest G4Opticks_5000.gdml muon_noIO.mac >& 5000_noIO.log
+export G4OPTICKS_DEBUG='--rngmax 10'
+time ./G4OpticksTest G4Opticks_50.gdml muon_noIO.mac >& 50_noIO_10.log
+time ./G4OpticksTest G4Opticks_100.gdml muon_noIO.mac >& 100_noIO_10.log
+time ./G4OpticksTest G4Opticks_5000.gdml muon_noIO.mac >& 5000_noIO_10.log
+time ./G4OpticksTest G4Opticks_10000.gdml muon_noIO.mac >& 10000_noIO_10.log
+time ./G4OpticksTest G4Opticks_50000.gdml muon_noIO.mac >& 50000_noIO_10.log
+export G4OPTICKS_DEBUG='--rngmax 10 --rtx 1'
+time ./G4OpticksTest G4Opticks_50.gdml muon_noIO.mac >& 50_noIO_10rtx.log
+time ./G4OpticksTest G4Opticks_100.gdml muon_noIO.mac >& 100_noIO_10rtx.log
+time ./G4OpticksTest G4Opticks_5000.gdml muon_noIO.mac >& 5000_noIO_10rtx.log
+time ./G4OpticksTest G4Opticks_10000.gdml muon_noIO.mac >& 10000_noIO_10rtx.log
+time ./G4OpticksTest G4Opticks_50000.gdml muon_noIO.mac >& 50000_noIO_10rtx.log
+export G4OPTICKS_DEBUG='--rngmax 100'
+time ./G4OpticksTest G4Opticks_50.gdml muon_noIO.mac >& 50_noIO_100.log
+time ./G4OpticksTest G4Opticks_100.gdml muon_noIO.mac >& 100_noIO_100.log
+time ./G4OpticksTest G4Opticks_5000.gdml muon_noIO.mac >& 5000_noIO_100.log
+time ./G4OpticksTest G4Opticks_10000.gdml muon_noIO.mac >& 10000_noIO_100.log
+time ./G4OpticksTest G4Opticks_50000.gdml muon_noIO.mac >& 50000_noIO_100.log
+export G4OPTICKS_DEBUG='--rngmax 100 --rtx 1'
+time ./G4OpticksTest G4Opticks_50.gdml muon_noIO.mac >& 50_noIO_100rtx.log
+time ./G4OpticksTest G4Opticks_100.gdml muon_noIO.mac >& 100_noIO_100rtx.log
+time ./G4OpticksTest G4Opticks_5000.gdml muon_noIO.mac >& 5000_noIO_100rtx.log
+time ./G4OpticksTest G4Opticks_10000.gdml muon_noIO.mac >& 10000_noIO_100rtx.log
+time ./G4OpticksTest G4Opticks_50000.gdml muon_noIO.mac >& 50000_noIO_100rtx.log

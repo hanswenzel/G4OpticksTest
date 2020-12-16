ae48de205c0c1f382f0a70728ebdd2e581dfb88a
Hans Wenzel <wenzel@fnal.gov>
2020-12-15 21:04:45 -0600

    make calls to G4Opticks::setSensorData for each of your sensors to avoid this, defining efficiencies and angular efficiency categories and identifiers for them.

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -34,15 +34,39 @@ RunAction::RunAction()
 
 void RunAction::BeginOfRunAction(const G4Run*) {
     if (ConfigurationManager::getInstance()->isEnable_opticks()) {
-#ifdef WITH_OPTICKS
-        G4cout << "\n\n###[ RunAction::BeginOfRunAction G4Opticks.setGeometry\n\n" << G4endl;
-        G4VPhysicalVolume* world = G4TransportationManager::GetTransportationManager()->GetNavigatorForTracking()->GetWorldVolume();
-        assert(world);
-        //bool standardize_geant4_materials = true; // required for alignment 
-        bool standardize_geant4_materials = false; // required for alignment 
-        G4Opticks::Get()->setGeometry(world, standardize_geant4_materials);
-        G4cout << "\n\n###] RunAction::BeginOfRunAction G4Opticks.setGeometry\n\n" << G4endl;
-#endif
+      //#ifdef WITH_OPTICKS
+      //  G4cout << "\n\n###[ RunAction::BeginOfRunAction G4Opticks.setGeometry\n\n" << G4endl;
+      //  G4VPhysicalVolume* world = G4TransportationManager::GetTransportationManager()->GetNavigatorForTracking()->GetWorldVolume();
+      //  assert(world);
+      //  //bool standardize_geant4_materials = true; // required for alignment 
+      //  bool standardize_geant4_materials = false; // required for alignment 
+      //  G4Opticks::Get()->setGeometry(world, standardize_geant4_materials);
+      //  G4cout << "\n\n###] RunAction::BeginOfRunAction G4Opticks.setGeometry\n\n" << G4endl;
+      //#endif
+ #ifdef WITH_OPTICKS
+     G4cout << "\n\n###[ RunAction::BeginOfRunAction G4Opticks.setGeometry\n\n" << G4endl ;
+     G4VPhysicalVolume* world = G4TransportationManager::GetTransportationManager()->GetNavigatorForTracking()->GetWorldVolume() ;
+     assert( world ) ;
+     bool standardize_geant4_materials = false ;   // required for alignment
+
+     G4Opticks* g4ok = G4Opticks::Get();
+     g4ok->setGeometry(world, standardize_geant4_materials );
+
+     const std::vector<G4PVPlacement*>& sensor_placements = g4ok->getSensorPlacements() ;
+     //assert( sensor_placements.size() == 1 );
+     G4cout << "sensor_placements.size():  "<<sensor_placements.size()<<G4endl;
+     for(unsigned i=0 ; i < sensor_placements.size()  ; i++)
+     {
+         float efficiency_1 = 0.5f ;
+         float efficiency_2 = 1.0f ;
+         int sensor_cat = -1 ;   // -1:means no angular info
+         int sensor_identifier = 0xc0ffee + i ;  // mockup a detector specific identifier
+         unsigned sensorIndex = 1+i ;  // 1-based
+         g4ok->setSensorData( sensorIndex, efficiency_1, efficiency_2, sensor_cat, sensor_identifier );
+     }
+
+     G4cout << "\n\n###] RunAction::BeginOfRunAction G4Opticks.setGeometry\n\n" << G4endl ;
+ #endif	
     }
 }
 
